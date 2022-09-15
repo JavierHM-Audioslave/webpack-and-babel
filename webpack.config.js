5f69a63977b92d3f49c1b5dac5f5f03ab72e65bf
Javier Monardo <javier.monardo@externos-cl.cencosud.com>
2022-09-15 17:17:50 -0300

    Commanding Webpack to use the Babel compiler in its bundling process when "npm run build" command is run

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
