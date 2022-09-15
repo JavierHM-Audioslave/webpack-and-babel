115ae56139b194f661126bb479c7b957a03f59bd
Javier Monardo <javier.monardo@externos-cl.cencosud.com>
2022-09-15 17:36:39 -0300

    Through the "index.js" file I write a React project sample in order to see React, Webpack and Babel working successfully toghether. This is properly working because when I double-click the "index.html" file after running the command "npm run build", I see "Hola Mundo con Webpack y Babel" in the browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Funciona!");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<h1>Hola Mundo con Webpack y Babel!</h1>);
